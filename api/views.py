2c3030d7edfb5b730bf4b5da323ba172c7f2886b
Ashf5 <friedash85@gmail.com>
2023-05-07 19:31:50 +0300

    First commit for the capstone project

---
+++ api/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, DestroyAPIView 
+from rest_framework.viewsets import ModelViewSet 
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Booking, Menu
+from .serializers import BookingSerializer, MenuSerializer
+
+# Create your views here.
+class MenuItemsView(ListCreateAPIView):
+    queryset = Menu.objects.all() 
+    serializer_class = MenuSerializer 
+
+class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
+    queryset = Menu.objects.all() 
+    serializer_class = MenuSerializer
+
+class BookingViewSet(ModelViewSet):
+    queryset = Booking.objects.all() 
+    serializer_class = BookingSerializer 
+    
